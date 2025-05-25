8342d2a893cb69cbdaba513378ed74c2d1442668
soham901 <sagathiyasoham12345@gmail.com>
2025-05-25 12:27:59 +0530

    fix

--- utils.py
+++ utils.py
@@ -2,9 +2,7 @@ import os
 
 FOLDER = 'lab-20'
 
-definitions = """1. WAP to demonstrate single inheritance, multilevel inheritance and hierarchical inheritance. (A)
-2. Create a class named shape. In this class, we have three subclasses circle, triangle and square. WAP to display area of all three classes. (B)
-3. WAP for implementing single inheritance which creates one class account_details for getting account information and another class interest for calculating and displaying total interest from the data inserted from account details. (C)""".split('\n')
+definitions = """""".split('\n')
 
 # create the directory if it doesn't exist
 if not os.path.exists(FOLDER):
