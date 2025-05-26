cc39cd99b72d8e09b321a455c1c901798aa6487c
Soham <sagathiyasoham12345@gmail.com>
2025-05-26 15:00:49 +0530

    refactor: Improve output formatting

--- lab-19/VowelCounter.java
+++ lab-19/VowelCounter.java
@@ -9,7 +9,7 @@ public class VowelCounter {
         int totalA = 0, totalE = 0, totalI = 0, totalO = 0, totalU = 0;
 
         while (true) {
-            System.out.print("Enter a sentence (or type 'quit' to exit): ");
+            System.out.print("Enter a sentence (or 'quit' to exit): ");
             String input = sc.nextLine();
 
             if (input.equalsIgnoreCase("quit")) {
@@ -28,22 +28,10 @@ public class VowelCounter {
                 }
             }
 
-            System.out.println("Vowel count in this sentence:");
-            System.out.println("a: " + countA);
-            System.out.println("e: " + countE);
-            System.out.println("i: " + countI);
-            System.out.println("o: " + countO);
-            System.out.println("u: " + countU);
-            System.out.println("------------------------------");
+            System.out.printf("This sentence - a:%d e:%d i:%d o:%d u:%d\n", countA, countE, countI, countO, countU);
         }
 
-        System.out.println("\nTotal vowel count in all sentences:");
-        System.out.println("a: " + totalA);
-        System.out.println("e: " + totalE);
-        System.out.println("i: " + totalI);
-        System.out.println("o: " + totalO);
-        System.out.println("u: " + totalU);
-
+        System.out.printf("\nTotal - a:%d e:%d i:%d o:%d u:%d\n", totalA, totalE, totalI, totalO, totalU);
         sc.close();
     }
 }
